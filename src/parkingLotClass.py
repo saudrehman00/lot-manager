2c707ab533ee5d2cd2988de3ba898ca9b372e579
Nabeel Siddiqui <nsiddi24@uwo.ca>
2022-11-30 16:52:29 -0500

    Basic Functionality For Client Side

--- src/parkingLotClass.py
+++
@@ -1,9 +0,0 @@
-class parkingLot:
-    def __init__(availableSpots, occupiedSpots):
-        self.availableSpots = availableSpots
-        self.occupiedSpots = occupiedSpots
-    
-    def occupySpot():
-
-    def freeSpot():
-
--- src/parkingManagerClass.py
+++
@@ -1,9 +0,0 @@
-class parkingManager:
-    def __init__(collectedMoney):
-        self.collectedMoney = collectedMoney
-    
-    def addSpot():
-
-    def removeSpot():
-
-    def setRate():
