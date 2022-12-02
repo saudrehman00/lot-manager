ac39b7d36d2b5a1c8598249880d6175b808aea24
saudrehman00 <saudrehman00@hotmail.com>
2022-12-01 20:44:40 -0500

    application forces unique lot names

--- src/ManagerPortal.py
+++ src/ManagerPortal.py
@@ -4,19 +4,21 @@ import os
 import sys
 from ManagerParkingLot import *
 
+
 class ManagerPortal:
     db = None
     connection = None
     parkingLots = dict()
 
-    def __init__(self, dbhost,  dbuser, dbpswd,manageruname,managerpswd):
-        self.db = MySQLdb.connect(host=dbhost,user=dbuser, passwd=dbpswd )
+    def __init__(self, dbhost,  dbuser, dbpswd, manageruname, managerpswd):
+        self.db = MySQLdb.connect(host=dbhost, user=dbuser, passwd=dbpswd)
         self.connection = self.db.cursor(MySQLdb.cursors.DictCursor)
         self.connection.execute("SHOW DATABASES;")
         result = self.connection.fetchall()
         if "lotmanager" in [database['Database'] for database in result]:
             self.connection.execute("USE lotmanager;")
-            self.connection.execute(f"SELECT fullname, password from manager WHERE username LIKE '{manageruname}'")
+            self.connection.execute(
+                f"SELECT fullname, password from manager WHERE username LIKE '{manageruname}'")
             result = self.connection.fetchone()
             if result['password'] == sha256(managerpswd.encode()).hexdigest():
                 print(f"HELLO {result['fullname']}")
@@ -26,7 +28,6 @@ class ManagerPortal:
         else:
             print("database has not been set up")
             sys.exit(1)
-
 
     def __del__(self):
         if self.connection:
@@ -94,12 +95,14 @@ GROUP  BY parkinglot.id, \
           r2.overtimerate"
         result = self.connection.execute(lotSQL)
         for lot in self.connection.fetchall():
-            self.parkingLots[lot['name']] = ManagerParkingLot(lot['id'],lot['name'],lot['occupiedspots'],lot['totalspaces'],lot['rate'],lot['overtimerate'],0)
+            self.parkingLots[lot['name']] = ManagerParkingLot(
+                lot['id'], lot['name'], lot['occupiedspots'], lot['totalspaces'], lot['rate'], lot['overtimerate'], 0)
         result = self.connection.execute(sqlBilling)
         for lot in self.connection.fetchall():
-            self.parkingLots[lot['name']] = ManagerParkingLot(lot['id'],lot['name'],lot['occupiedspots'],lot['totalspaces'],lot['rate'],lot['overtimerate'],lot['revenuegenerated'])
+            self.parkingLots[lot['name']] = ManagerParkingLot(
+                lot['id'], lot['name'], lot['occupiedspots'], lot['totalspaces'], lot['rate'], lot['overtimerate'], lot['revenuegenerated'])
 
-    def setRate(self, name,rate):
+    def setRate(self, name, rate):
         lotid = self.parkingLots[name].getlotID()
         overtimeRate = self.parkingLots[name].getRate()
         updateOldRate = f"UPDATE rates SET expirydate=NOW() WHERE lotid='{lotid}'"
@@ -108,7 +111,7 @@ GROUP  BY parkinglot.id, \
         self.connection.execute(insertNewRate)
         self.db.commit()
 
-    def setOvertimeRate(self, name,overtimerate):
+    def setOvertimeRate(self, name, overtimerate):
         lotid = self.parkingLots[name].getlotID()
         rate = self.parkingLots[name].getRate()
         updateOldRate = f"UPDATE rates SET expirydate=NOW() WHERE lotid='{lotid}'"
@@ -116,8 +119,8 @@ GROUP  BY parkinglot.id, \
         self.connection.execute(updateOldRate)
         self.connection.execute(insertNewRate)
         self.db.commit()
-    
-    def setBothRate(self,name,rate,overtimerate):
+
+    def setBothRate(self, name, rate, overtimerate):
         lotid = self.parkingLots[name].getlotID()
         updateOldRate = f"UPDATE rates SET expirydate=NOW() WHERE lotid='{lotid}'"
         insertNewRate = f"INSERT INTO rates (lotid, rate, overtimerate) VALUES ('{lotid}', '{rate}', '{overtimerate}')"
@@ -125,19 +128,19 @@ GROUP  BY parkinglot.id, \
         self.connection.execute(insertNewRate)
         self.db.commit()
 
-    def getOccupancy(self,name):
+    def getOccupancy(self, name):
         self.updateLotList()
         return self.parkingLots[name].getOccupancyRate()
 
-    def getRate(self,name):
+    def getRate(self, name):
         self.updateLotList()
         return self.parkingLots[name].getRate()
 
-    def getOvertimeRate(self,name):
+    def getOvertimeRate(self, name):
         self.updateLotList()
         return self.parkingLots[name].getOvertimeRate()
 
-    def getRevenueGenerated(self,name):
+    def getRevenueGenerated(self, name):
         self.updateLotList()
         return self.parkingLots[name].getPastMonthRevenue()
 
@@ -145,14 +148,19 @@ GROUP  BY parkinglot.id, \
         self.updateLotList()
         return list(self.parkingLots.keys())
 
-    def createNewLot(self,name,floors,spaceperfloor,standardrate,overtimerate):
+    def createNewLot(self, name, floors, spaceperfloor, standardrate, overtimerate):
+        uniquemanagersql = f"SELECT * FROM parkinglot WHERE name like '{name}'"
+        self.connection.execute(uniquemanagersql)
+        if self.connection.fetchall():
+            print("Lot name is not unique. Please pick a different name")
+            return
         createlotsql = f"INSERT INTO parkinglot (name,numfloors,numspaces) VALUES ('{name}','{floors}','{spaceperfloor}')"
         self.connection.execute(createlotsql)
         setratesql = f"INSERT INTO rates (lotid, rate, overtimerate) VALUES ('{self.connection.lastrowid}','{standardrate}','{overtimerate}')"
         self.connection.execute(setratesql)
         self.db.commit()
 
-    def createNewManager(self,fullname,username,password):
+    def createNewManager(self, fullname, username, password):
         uniquemanagersql = f"SELECT * FROM manager WHERE username like '{username}'"
         self.connection.execute(uniquemanagersql)
         if self.connection.fetchall():
@@ -161,5 +169,3 @@ GROUP  BY parkinglot.id, \
         createmanagersql = f"INSERT INTO manager (fullname, username, password) VALUES ('{fullname}', '{username}', '{sha256(password.encode()).hexdigest()}')"
         self.connection.execute(createmanagersql)
         self.db.commit()
-
-    
