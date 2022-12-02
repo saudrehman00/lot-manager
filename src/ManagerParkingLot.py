9f3a50e6d8b7e16bde76050bc12d82d6d4197413
saudrehman00 <saudrehman00@hotmail.com>
2022-12-01 20:53:43 -0500

    comment manager client

--- src/ManagerParkingLot.py
+++ src/ManagerParkingLot.py
@@ -1,31 +1,34 @@
 class ManagerParkingLot:
-    def __init__(self, lotid, name,occupiedspots,totalSpots,rate,overtimerate,pastmonthrevenue):
+    def __init__(self, lotid, name, occupiedspots, totalSpots, rate, overtimerate, pastmonthrevenue):
         self.lotid = lotid
         self.name = name
-        self.occupiedSpots =occupiedspots
+        self.occupiedSpots = occupiedspots
         self.totalspots = totalSpots
         self.rate = rate
         self.overtimeRate = overtimerate
         self.pastmonthrevenue = pastmonthrevenue
-    
+
+    # return lot id
     def getlotID(self):
         return self.lotid
-    
+
+    # return lot name
     def getName(self):
         return self.name
 
+    # return current rate
     def getRate(self):
         return self.rate
 
+    # return current overtime rate
     def getOvertimeRate(self):
         return self.overtimeRate
 
+    # return past month revenue
     def getPastMonthRevenue(self):
         return self.pastmonthrevenue
 
+    # return current occupancy percentage
     def getOccupancyRate(self):
         return (self.occupiedSpots / (self.totalspots * 1.0)) * 100
-    
-    # def __str__(self):
-    #     return f""
 
